21164ddb05ff3012aef54d77e51923c0ec1915b8
Manula Dharmawardana <md920@cam.ac.uk>
2022-02-08 20:25:59 +0000

    Complete Task 1C

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -9,8 +9,18 @@ from haversine import haversine, Unit
 from os import stat
 
 def stations_by_distance(stations, p):
+    """Requirement for Task 1B"""
     dist_stat = []
     for stat in stations:
         dist_stat.append((stat, haversine(stat.coord, p),))
     result = sorted_by_key(dist_stat, 1)
     return result
+
+def stations_within_radius(stations, centre, r):
+    """Requirement for Task 1C"""
+    stat_dist = stations_by_distance(stations, centre)
+    stats_in_rad = []
+    for elem in stat_dist:
+        if elem[1] < r:
+            stats_in_rad.append(elem[0])
+    return stats_in_rad
