30d3a7c63d2dab1a681a9b789775bf420e68540d
Manula Dharmawardana <md920@cam.ac.uk>
2022-02-08 19:30:42 +0000

    Complete Task1B

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -2,8 +2,15 @@
 #
 # SPDX-License-Identifier: MIT
 """This module contains a collection of functions related to
-geographical data.
-
-"""
+geographical data."""
 
 from .utils import sorted_by_key  # noqa
+from haversine import haversine, Unit
+from os import stat
+
+def stations_by_distance(stations, p):
+    dist_stat = []
+    for stat in stations:
+        dist_stat.append((stat, haversine(stat.coord, p),))
+    result = sorted_by_key(dist_stat, 1)
+    return result
